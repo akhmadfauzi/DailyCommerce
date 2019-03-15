739597183efcb676bda1b81c7fa996be12721c64
fauzi <sifauzi@gmail.com>
2019-03-15 07:04:31 +0700

    Update

--- src/components/Pos.js
+++ src/components/Pos.js
@@ -3,8 +3,39 @@ import React, { Component } from 'react';
 class Pos extends Component {
 	render() {
 		return (
-			<div className="pos-content" style={{background:'black'}}>
-				this is pos
+			<div className="pos-content">
+				<div className="left pos-calculator">
+					<div className="head left-head">
+						<input type="text" placeholder="Find products"/>
+						<input type="text" placeholder="Find products"/>
+					</div>
+					<div className="body">
+					 <table className="pos-cart">
+						 <thead>
+							<tr>
+								<th>Product</th>
+								<th>Price</th>
+								<th>Qty</th>
+								<th>Subtotal</th>
+								<th>Delete</th>
+							</tr>
+						 </thead>
+						 <tbody>
+						 <tr>
+								<th>Product</th>
+								<th>Price</th>
+								<th>Qty</th>
+								<th>Subtotal</th>
+								<th>Delete</th>
+							</tr>
+						 </tbody>
+					 </table>
+					</div>
+					<div className="footer"></div>
+				</div> 
+				<div className="right products-list">
+					product list
+				</div>				
 			</div>
 		);
 	}
